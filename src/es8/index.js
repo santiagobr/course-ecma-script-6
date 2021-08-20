5bcc76d25eda336a0ebc83d365946fa3fe7b62ce
santiagobr <santiagobastidasrodriguez@gmail.com>
2021-08-20 09:41:09 -0500

    class seventh: Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -1,3 +1,5 @@
+import hello from "../es6/module"
+
 /* Convertir un objeto de elementos a una matriz */
 const data = {
   frontend: 'santiago',
@@ -28,6 +30,35 @@ console.log(string.padStart(7, 'hi'))
 console.log(string.padEnd(12, 'hi'))
 
 
+/* Trailing commas */
 const obj = {
   name: 'santiago'
 }
+
+/* Async Await */
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (false)
+      ? setTimeout(() => resolve('Hello World'), 3000)
+      : reject(new Error('Test Error!'))
+  })
+}
+
+const helloAsync = async () => {
+  const hello = await helloWorld()
+  console.log(hello)
+}
+
+helloAsync()
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld()
+    console.log(hello)
+  } catch (error) {
+    console.log(Error)
+  }
+}
+
+anotherFunction()
